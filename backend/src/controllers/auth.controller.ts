51381412e72e69e3684b3a7d0400371897cfba45
Yolanda Mejane <mysivuyile@gmail.com>
2025-05-03 19:04:53 +0200

    "installed backend dependencies"

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,7 +1,31 @@
 import { Request, Response } from 'express';
+import prisma from "../db/prisma.js";
+import bcryptjs from "bcryptjs";
 
 export const signup = async (req:Request, res:Response) => {
-    
+    try {
+        const {fullName, username, password, confirmPassword, gender} = req.body
+
+        if (!fullName || !username || !password || confirmPassword || !gender) {
+            return res.status(400).json({ error: "Please fill in all fields" });
+        }
+
+        if (password !== confirmPassword) {
+            return res.status(400).json({ error: "Passwords don't match" });
+        }
+
+        const user = await prisma.user.findUnique({ where:{username }});
+        
+        if (user) {
+            return res.status(400).json({ error: "Username already exists" });
+        }
+
+        const salt = await bcryptjs.genSalt(10);
+        const hashedPassword = await bcryptjs.hash(password, salt);
+
+    } catch (error) {
+
+    }
 }
 export const login = async (req:Request, res:Response) => {}
 export const logout = async (req:Request, res:Response) => {}
